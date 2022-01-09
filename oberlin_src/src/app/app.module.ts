46813ead14d879ccc2f26fde9376772fadb0eadd
29574196 <29574196@student.g.nwu.ac.za>
2022-01-09 15:18:41 +0200

    latest design

---
+++ oberlin_src/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {PreloaderComponent} from './layout/preloader/preloader.component';
+import {NavComponent} from './layout/nav/nav.component';
+import {FooterComponent} from './layout/footer/footer.component';
+import { Index2Component } from './pages/index2/index2.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    PreloaderComponent,
+    NavComponent,
+    FooterComponent,
+    Index2Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
